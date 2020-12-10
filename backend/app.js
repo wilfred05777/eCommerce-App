b49251e39bfcf1859f9279dc6ae1fd60d886d848
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2020-12-11 06:47:18 +0800

    Access-Control-Allow-Headers

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,6 @@ const logger = require('morgan');
 const cors = require('cors');
 const app = express();
 
-
 // IMPORT ROUTES
 const indexRouter = require('./routes/index');
 const productsRoute = require('./routes/products');
@@ -31,43 +30,6 @@ app.use((req,res, next)=>{
     next();
 });
 
-// app.use(cors({
-//   allowedHeaders: 'Content-type, Authorization, Origin, X-Requested-With, Accept',
-//   origin: "*",
-//   methods:['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
-// }));
-
-
-
-// app.use((req, res, next) =>{
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   // res.header('Access-Control-Allow-Origin','*'),
-//   // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE' ),
-//   // res.header('Acess-Control-Allow-Headers', 'X-Requested-With, content-type'),
-//   // res.header('Access-Control-Allow-Credentials', true)
-//   next();
-
-//   app.options('*', (req, res)=>{
-//     res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-    
-//     res.send();
-//   });
-// });
-
-// app.use(cors(
-//   response.setHeader('Access-Control-Allow-Origin','*'),
-//   response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE' ),
-//   response.setHeader('Acess-Control-Allow-Headers', 'X-Requested-With, content-type'),
-//   response.setHeader('Access-Control-Allow-Credentials', true)
-// ));
-
-// app.use(function(req, res, next){
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -77,8 +39,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
