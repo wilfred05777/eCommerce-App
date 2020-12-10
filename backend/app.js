4d6cf046b42ac0d8439ee0ed5d60600d9d8cc579
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2020-12-11 06:46:42 +0800

    Access-Control-Allow-Headers

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use('/api/orders', ordersRoute);
 
 app.use((req,res, next)=>{
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PATCH, DELETE, OPTIONS"
